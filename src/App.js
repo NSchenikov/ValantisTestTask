bbeb9ecdf40246a60c6c0e66d28b328b1678c933
Nikita-Schenikov <nikita-shenikov@mail.ru>
2024-02-25 23:27:48 +0300

    now get list of items from API

--- src/App.js
+++ src/App.js
@@ -1,57 +1,36 @@
-import { useEffect } from "react";
-import axios from "axios";
-import md5 from "md5";
+import { useEffect, useState } from "react";
+import { getIds, getItems } from "./api";
+
 import "./App.css";
 
 function App() {
-  const baseUrl = "https://api.valantis.store:41000/";
+
+  const [ids, setIds] = useState([])
+  const [items, setItems] = useState([])
 
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        let now = new Date();
-        let yyyy = now.getFullYear().toString();
-        let mm =
-          (now.getMonth() + 1).toString().length === 1
-            ? "0" + (now.getMonth() + 1).toString()
-            : (now.getMonth() + 1).toString();
-        let dd =
-          now.getDate().toString().length === 1
-            ? "0" + now.getDate().toString()
-            : now.getDate().toString();
-        let date = yyyy + mm + dd;
-        console.log(date);
-
-        let password = "Valantis";
-        let authString = md5(password + "_" + date);
-        console.log(authString);
-        const headers = {
-          "X-Auth": authString,
-          "Content-Type": "application/json",
-        };
-
-        const requestData = {
-          "action": "get_ids",
-          "params": { "offset": 10, "limit": 50 },
-        };
-
-        const response = await axios.post(baseUrl, requestData, {
-          headers: headers,
-        });
-
-        console.log(response.data);
-      } catch (error) {
-        if (error.response && error.response.status === 401) {
-          console.error(
-            "Ошибка аутентификации: Некорректная авторизационная строка"
-          );
-        } else {
-          console.error("Ошибка при получении данных: ", error);
-        }
-      }
-    };
-    fetchData();
+    getIds()
+      .then((res) => {
+        setIds(res.result);
+        return res.result;
+      })
+      .then((res) => {
+        // console.log(res);
+        getItems(res)
+          .then((res) => {
+            setItems(res.result);
+            return res;
+          });
+      });
   }, []);
+
+  useEffect(() => {
+    console.log(ids);
+  }, [ids])
+
+  useEffect(() => {
+    console.log(items);
+  }, [items])
 
   return (
     <div className="App">
---
+++ src/api.js
@@ -0,0 +1,83 @@
+import axios from "axios";
+import md5 from "md5";
+const baseUrl = "https://api.valantis.store:41000/";
+let password = "Valantis";
+
+let now = new Date();
+let yyyy = now.getFullYear().toString();
+let mm =
+  (now.getMonth() + 1).toString().length === 1
+    ? "0" + (now.getMonth() + 1).toString()
+    : (now.getMonth() + 1).toString();
+let dd =
+  now.getDate().toString().length === 1
+    ? "0" + now.getDate().toString()
+    : now.getDate().toString();
+let date = yyyy + mm + dd;
+// console.log(date);
+let authString = md5(password + "_" + date);
+
+export const getIds = () => {
+
+    // console.log(authString);
+    const headers = {
+      "X-Auth": authString,
+      "Content-Type": "application/json",
+    };
+    const requestData = {
+      "action": "get_ids",
+      "params": {
+        "offset": 0,
+        "limit": 50,
+      },
+    };
+  
+    return axios.post(baseUrl, requestData, { headers: headers })
+      .then(response => {
+        return response.data;
+      })
+      .then(response => {
+
+        return response;
+      })
+      .catch(error => {
+        if (error.response && error.response.status === 401) {
+          console.error("Ошибка аутентификации: Некорректная авторизационная строка");
+        } else {
+          console.error("Ошибка при получении данных: ", error);
+        }
+        throw error;
+      });
+  };
+
+  export const getItems = (ids) => {
+
+    // console.log(authString);
+    const headers = {
+      "X-Auth": authString,
+      "Content-Type": "application/json",
+    };
+    const requestData = {
+      "action": "get_items",
+      "params": {
+        "ids": ids
+      },
+    };
+  
+    return axios.post(baseUrl, requestData, { headers: headers })
+      .then(response => {
+        return response.data;
+      })
+      .then(response => {
+        // console.log(response);
+        return response;
+      })
+      .catch(error => {
+        if (error.response && error.response.status === 401) {
+          console.error("Ошибка аутентификации: Некорректная авторизационная строка");
+        } else {
+          console.error("Ошибка при получении данных: ", error);
+        }
+        throw error;
+      });
+  };
